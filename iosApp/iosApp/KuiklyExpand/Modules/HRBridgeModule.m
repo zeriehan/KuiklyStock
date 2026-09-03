d49396a5ee4575051a4f17188f2fe71abc784ec1
Z <1835938802@qq.com>
2026-09-03 21:35:01 +0800

    feat(ios): 选取文字改为原生可选中UITextView（对齐Android，长文内部滚动）

--- iosApp/iosApp/KuiklyExpand/Modules/HRBridgeModule.m
+++ iosApp/iosApp/KuiklyExpand/Modules/HRBridgeModule.m
@@ -26,13 +26,34 @@
     NSDictionary *params = [args[KR_PARAM_KEY] hr_stringToDictionary];
     NSString *text = params[@"text"];
     NSString *title = params[@"title"] ?: @"选取文字";
-    if (text.length > 0) {
-        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-        pasteboard.string = text;
+    if (text.length == 0) {
+        return;
     }
+    // 真可选中文本：Alert 内嵌 UITextView（selectable，左对齐，自动换行，长文内部滚动）
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
-                                                                     message:text
-                                                              preferredStyle:UIAlertControllerStyleAlert];
+                                                                  message:nil
+                                                           preferredStyle:UIAlertControllerStyleAlert];
+    UITextView *textView = [[UITextView alloc] init];
+    textView.translatesAutoresizingMaskIntoConstraints = NO;
+    textView.editable = NO;
+    textView.selectable = YES;
+    textView.dataDetectorTypes = UIDataDetectorTypeNone;
+    textView.font = [UIFont systemFontOfSize:16];
+    textView.text = text;
+    textView.textAlignment = NSTextAlignmentLeft;
+    textView.backgroundColor = [UIColor clearColor];
+    textView.layoutManager.allowsNonContiguousLayout = NO;
+    [alert.view addSubview:textView];
+
+    CGFloat screenW = [UIScreen mainScreen].bounds.size.width;
+    CGFloat screenH = [UIScreen mainScreen].bounds.size.height;
+    [textView.leadingAnchor constraintEqualToAnchor:alert.view.leadingAnchor constant:16].active = YES;
+    [textView.trailingAnchor constraintEqualToAnchor:alert.view.trailingAnchor constant:-16].active = YES;
+    [textView.topAnchor constraintEqualToAnchor:alert.view.topAnchor constant:50].active = YES;
+    [textView.bottomAnchor constraintEqualToAnchor:alert.view.bottomAnchor constant:-50].active = YES;
+    [textView.widthAnchor constraintEqualToConstant:screenW - 64].active = YES;
+    [textView.heightAnchor constraintLessThanOrEqualToConstant:screenH * 0.6].active = YES;
+
     [alert addAction:[UIAlertAction actionWithTitle:@"关闭" style:UIAlertActionStyleDefault handler:nil]];
     UIViewController *vc = [UIApplication sharedApplication].keyWindow.rootViewController;
     [vc presentViewController:alert animated:YES completion:nil];
